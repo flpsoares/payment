8599c144f35bace42aaf32d0dd9bb52fe7f57a94
Filipe Soares <filipeseventeen1@gmail.com>
2022-03-14 17:58:24 -0300

    Adicionada a função de atualização automática

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { StatusBar } from 'expo-status-bar'
 
@@ -6,7 +6,23 @@ import { NavigationContainer } from '@react-navigation/native'
 import { Routes } from './src/routes'
 import { NavigateProvider } from './src/contexts/NavigateContext'
 
+import * as Updates from 'expo-updates'
+
 export default function App() {
+  useEffect(() => {
+    const updateApp = async () => {
+      const { isAvailable } = await Updates.checkForUpdateAsync()
+
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync()
+
+        await Updates.reloadAsync()
+      }
+    }
+
+    updateApp()
+  }, [])
+
   return (
     <NavigationContainer>
       <NavigateProvider>
